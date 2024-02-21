19449fdec09e18af0964819a8e3628ee6ae66ff3
famesjranko <andrewmcdonald42@gmail.com>
2024-02-22 00:16:58 +1100

    Update obj_det_and_trk_streamlit.py

--- obj_det_and_trk_streamlit.py
+++ obj_det_and_trk_streamlit.py
@@ -18,8 +18,10 @@ import torch.backends.cudnn as cudnn
 from utils.general import set_logging
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
-from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
-                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.general import (LOGGER, Profile, check_file, check_img_size, 
+                            check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args,
+                            scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 
@@ -33,7 +35,19 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))
 import skimage
 from sort import *
 
-#............................... Tracker Functions ............................
+
+#-----------Object Blurring-------------------
+blurratio = 40
+
+
+#.................. Tracker Functions .................
+'''Computer Color for every box and track'''
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+def compute_color_for_labels(label):
+    color = [int(int(p * (label ** 2 - label + 1)) % 255) for p in palette]
+    return tuple(color)
+
+
 """" Calculates the relative bounding box from absolute pixel values. """
 def bbox_rel(*xyxy):
     bbox_left = min([xyxy[0].item(), xyxy[2].item()])
@@ -48,7 +62,12 @@ def bbox_rel(*xyxy):
 
 
 """Function to Draw Bounding boxes"""
-def draw_boxes(img, bbox, identities=None, categories=None, names=None,offset=(0, 0)):
+def draw_boxes(img, bbox, identities=None, categories=None, 
+                names=None, color_box=None,offset=(0, 0), mask_image=None):
+                
+    if mask_image is None:
+        mask_image = np.zeros_like(img)  # Create a mask if not provided
+        
     for i, box in enumerate(bbox):
         x1, y1, x2, y2 = [int(i) for i in box]
         x1 += offset[0]
@@ -58,49 +77,55 @@ def draw_boxes(img, bbox, identities=None, categories=None, names=None,offset=(0
         cat = int(categories[i]) if categories is not None else 0
         id = int(identities[i]) if identities is not None else 0
         data = (int((box[0]+box[2])/2),(int((box[1]+box[3])/2)))
-        label = "ID : * "+str(id)
-        (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-        cv2.rectangle(img, (x1, y1), (x2, y2), (255,191,0), 2)
-        cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), (255,191,0), -1)
-        cv2.putText(img, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.6, [255, 255, 255], 1)
-        cv2.circle(img, data, 6, (255,191,0),-1)
-    return img
+        label = str(id)
+        
+        # Correctly calculate the center_point using the current box
+        center_point = (int((x1 + x2) / 2), int((y1 + y2) / 2))
+        
+        #### =======================================================
+        # Draw circles on the mask image using the calculated center points
+        circle_radius = 5
+        circle_color = (0, 255, 0)  # Green color in BGR
+        cv2.circle(mask_image, center_point, circle_radius, circle_color, -1)  # -1 for filled circle
+        #### =======================================================
+
+        if color_box:
+            color = compute_color_for_labels(id)
+            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+            cv2.rectangle(img, (x1, y1), (x2, y2),color, 2)
+            cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), (255,191,0), -1)
+            cv2.putText(img, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.6, 
+            [255, 255, 255], 1)
+            cv2.circle(img, data, 3, color,-1)
+        else:
+            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+            cv2.rectangle(img, (x1, y1), (x2, y2),(255,191,0), 2)
+            cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), (255,191,0), -1)
+            cv2.putText(img, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.6, 
+            [255, 255, 255], 1)
+            cv2.circle(img, data, 3, (255,191,0),-1)
+    return img, mask_image
 #..............................................................................
 
 
 @torch.no_grad()
 def detect(weights=ROOT / 'yolov5n.pt',
         source=ROOT / 'yolov5/data/images', 
-        data=ROOT / 'yolov5/data/coco128.yaml',  
-        stframe=None,
+        data=ROOT / 'yolov5/data/coco128.yaml',
         kpi5_text="",
         kpi6_text="",
-        imgsz=(640, 640),
-        conf_thres=0.25,
-        
-        iou_thres=0.45,  
-        max_det=1000,  
-        device='cpu',  
-        view_img=False,  
-        save_txt=False, 
-        save_conf=False, 
-        save_crop=False, 
-        nosave=False,  
-        classes=None,  
-        agnostic_nms=False,  
-        augment=False,  
-        visualize=False,  
-        update=False,  
-        project=ROOT / 'runs/detect',  
-        name='exp',  
-        exist_ok=False,  
-        line_thickness=2,  
-        hide_labels=False,  
-        hide_conf=False,  
-        half=False, 
-        dnn=False,
-        display_labels=False,
-        ):
+        kpi7_text="",
+        imgsz=(640, 640), conf_thres=0.25, conf_thres_per_class=None,
+        iou_thres=0.45, max_det=1000, device='cpu',  view_img=True,  
+        save_txt=False, save_conf=False, save_crop=False, 
+        nosave=False, classes=None,  agnostic_nms=False,  
+        augment=False, visualize=False,  update=False,  
+        project=ROOT / 'runs/detect',  name='exp',  
+        exist_ok=False, line_thickness=2,hide_labels=False,  
+        hide_conf=False,half=False,dnn=False,display_labels=False,
+        blur_obj=False,color_box = False, stframe=None, stframe2=None,
+        save_zones=None, load_zones=None, blacked=None):
+    
     save_img = not nosave and not source.endswith('.txt') 
     
     #.... Initialize SORT .... 
@@ -110,6 +135,7 @@ def detect(weights=ROOT / 'yolov5n.pt',
     sort_tracker = Sort(max_age=sort_max_age,
                        min_hits=sort_min_hits,
                        iou_threshold=sort_iou_thresh) 
+    track_color_id = 0
     #......................... 
     
     
@@ -145,6 +171,10 @@ def detect(weights=ROOT / 'yolov5n.pt',
     
     dt, seen = [0.0, 0.0, 0.0], 0
     
+    mask_image = None
+    
+    tracked_dets = None
+    
     for path, im, im0s, vid_cap, s in dataset:
         
         t1 = time_sync()
@@ -174,6 +204,8 @@ def detect(weights=ROOT / 'yolov5n.pt',
                 s += f'{i}: '
             else:
                 p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+                
+            mask_image = np.zeros_like(im0)
 
             p = Path(p)
             save_path = str(save_dir / p.name)
@@ -184,12 +216,18 @@ def detect(weights=ROOT / 'yolov5n.pt',
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
             if len(det):
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
-                
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
-                    
-                
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if blur_obj:
+                        crop_obj = im0[int(xyxy[1]):int(xyxy[3]),int(xyxy[0]):int(xyxy[2])]
+                        blur = cv2.blur(crop_obj,(blurratio,blurratio))
+                        im0[int(xyxy[1]):int(xyxy[3]),int(xyxy[0]):int(xyxy[2])] = blur
+                    else:
+                        continue
                 #..................USE TRACK FUNCTION....................
                 #pass an empty array to sort
                 dets_to_sort = np.empty((0,6))
@@ -204,22 +242,37 @@ def detect(weights=ROOT / 'yolov5n.pt',
                 tracked_dets = sort_tracker.update(dets_to_sort)
                 tracks =sort_tracker.getTrackers()
                 
+
                 #loop over tracks
-                for track in tracks:
-                    #draw tracks
-                    [cv2.line(im0, (int(track.centroidarr[i][0]),int(track.centroidarr[i][1])), 
-                            (int(track.centroidarr[i+1][0]),int(track.centroidarr[i+1][1])),
-                            (124, 252, 0), thickness=3) for i,_ in  enumerate(track.centroidarr) 
-                            if i < len(track.centroidarr)-1 ] 
+                # for track in tracks:
+                    # if color_box:
+                        # color = compute_color_for_labels(track_color_id)
+                        # [cv2.line(im0, (int(track.centroidarr[i][0]),int(track.centroidarr[i][1])), 
+                                # (int(track.centroidarr[i+1][0]),int(track.centroidarr[i+1][1])),
+                                # color, thickness=3) for i,_ in  enumerate(track.centroidarr) 
+                                # if i < len(track.centroidarr)-1 ] 
+                        # track_color_id = track_color_id+1
+                    # else:
+                        # [cv2.line(im0, (int(track.centroidarr[i][0]),int(track.centroidarr[i][1])), 
+                                # (int(track.centroidarr[i+1][0]),int(track.centroidarr[i+1][1])),
+                                # (124, 252, 0), thickness=3) for i,_ in  enumerate(track.centroidarr) 
+                                # if i < len(track.centroidarr)-1 ] 
                 
                 # draw boxes for visualization
                 if len(tracked_dets)>0:
                     bbox_xyxy = tracked_dets[:,:4]
                     identities = tracked_dets[:, 8]
                     categories = tracked_dets[:, 4]
-                    draw_boxes(im0, bbox_xyxy, identities, categories, names)
-                
-        
+                    img, updated_mask_image  = draw_boxes(im0, bbox_xyxy, identities, categories, names, color_box, mask_image=mask_image)
+                    
+            kpi5_text.write(str(psutil.virtual_memory()[2])+"%")
+            kpi6_text.write(str(psutil.cpu_percent())+'%')
+            if tracked_dets is not None:
+                kpi7_text.write(str(len(tracked_dets)))
+            stframe.image(im0, channels="BGR",use_column_width=True)
+            
+            if blacked:
+                stframe2.image(mask_image, channels="BGR",use_column_width=True)
 
             if view_img:
                 cv2.imshow(str(p), im0)
@@ -241,18 +294,60 @@ def detect(weights=ROOT / 'yolov5n.pt',
                             save_path += '.mp4'
                         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer.write(im0)
-            
-        
-        kpi5_text.write(str(psutil.virtual_memory()[2])+"%")
-        kpi6_text.write(str(psutil.cpu_percent())+'%')
-        stframe.image(im0, channels="BGR",use_column_width=True)
-        
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
+        print("Frame Processing!")
+    print("Video Exported Success")
 
     if update:
         strip_optimizer(weights)
     
     if vid_cap:
         vid_cap.release()
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--blur-obj', action='store_true', help='Blur Detected Objects')
+    parser.add_argument('--color-box', action='store_true', help='Change color of every box and track')
+    parser.add_argument('--kpi5_text', default='', help='streemlit kpi5_text')
+    parser.add_argument('--kpi6_text', default='', help='streemlit kpi6_text')
+    parser.add_argument('--kpi7_text', default='', help='streemlit kpi7_text')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    print_args(vars(opt))
+    return opt
+
+
+def main(opt):
+    check_requirements(exclude=('tensorboard', 'thop'))
+    detect(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
